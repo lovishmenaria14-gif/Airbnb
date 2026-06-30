7dce53edff906b79074c4e76c1727603e33ae7da
Your Name <your-email@example.com>
2026-06-30 09:34:30 +0530

    update url

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const authDataContext = createContext();
 
 function AuthContext({ children }) {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://airbnb-1-s2he.onrender.com";
 
   const [user, setUser] = useState(null);
 
