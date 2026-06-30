d3fcdf58e022831af0f6f994b24acf52c39ec34f
Your Name <your-email@example.com>
2026-06-30 09:14:23 +0530

    add vercel link

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://airbnb-us8j.vercel.app/",
+    ],
     credentials: true,
   })
 );
